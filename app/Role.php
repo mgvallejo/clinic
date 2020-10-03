9a8a966d7e951f86b4530358d96cc8935aade912
miky <mgvallejo2012@gmail.com>
2020-10-03 13:42:59 -0500

    Relaciones de la tabla Rol
    Generando las relaciones para permissions y users.

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'slug', 'description',
+    ];
+
+//Relaciones
+    public function permissions(){
+        return $this->hasMany('App\Permission');
+    }
+
+    public function users(){
+        return $this->belongsToMany('App\User');
+    }
+//Almacenamiento
+
+//Validación
+
+//Recuperación de información
+
+//Otras operaciones
+
 }
